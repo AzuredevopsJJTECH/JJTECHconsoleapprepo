4ec5e52ec093243c8c8fc335630f1d00e722a0f0
JJTECH <hirannayyahassansuresh@gmail.com>
2023-08-11 06:21:57 +0530

    Made changes from VS

--- Program.vb
+++ Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello JJTECH World!")
+        Console.WriteLine("Hello JJTECH World Vibrant!")
     End Sub
 End Module
